2f0f2d3d3856b9a01d36462aeb929ed398033c9b
salman shamsi <99350382+Salmanshamsi@users.noreply.github.com>
2023-09-18 22:09:45 -0700

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -27,7 +27,7 @@ const app  = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(process.cwd + "/public" )));
+app.use(express.static(path.join(process.cwd() + "/public" )));
 
 
 const port = process.env.PORT || 3000;
